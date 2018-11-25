1aa8a047c2054f56fa8170ee5392b7362edb22dc
xuyuwei <1801220021@pku.edu.cn>
2018-11-25 23:44:02 +0800

    web界面+微信回复

---
+++ web天气/tianqiyubao.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+
+</head>
+<body>
+<div data-role="page" id="pageone">
+<div data-role="header" data-theme="b">
+    <a href="#" data-role="button" data-icon="search">搜索城市</a>
+    <h1>北京天气</h1>
+    <a href="#" data-role="button" data-icon="refresh">刷新</a>
+  </div>
+  <div data-role="content" style="background-image: url(images/biz_plugin_weather_shenzhen_bg.jpg);color:white">
+  <table>
+    <tr>
+      <th style="width:100">
+        <p><font size="10">北京</font></p>
+        <p><font size="5">14:05发布</font></p>
+        <p><font size="5">湿度：20%</font></p>
+      </th>
+        <td>
+        <p><font size="5">PM2.5</font></p>
+        <p><font size="5">162</font></p>
+        </td>
+        <td>
+        <img src="images/biz_plugin_weather_0_50.PNG">
+      </td>
+    </tr>
+    <tr>
+     <td>
+        <img src="images/biz_plugin_weather_qing.PNG">
+      </td>
+     <th style="width:100">
+        <p><font size="5">25日星期天</font></p>
+        <p><font size="5">晴</font></p>
+        <p><font size="5">15℃~25℃</font></p>
+        <p><font size="5">风力：25级</font></p>
+      </th>
+    </tr>
+    <tr>
+    <th>
+  <p><font size="15">6日天气</font></p>
+    </th>
+    </tr>
+</table>
+  
+  </div>
+
+  <div data-role="footer">
+  <h1>底部文本</h1>
+  </div>
+</div>
+
+</body>
+</html>
